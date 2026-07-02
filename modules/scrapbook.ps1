ecdc2d5def3c2a577a47ffaeaaeaa5a790126f49
Joel Quimper <joel_quimper@hotmail.com>
2026-07-02 17:45:47 -0400

    Update SharePoint module version in scrapbook.ps1

--- modules/scrapbook.ps1
+++ modules/scrapbook.ps1
@@ -6,7 +6,7 @@ $pathToModules = "C:\src\PowerShellModules"
 Save-Module -Name Microsoft.Online.SharePoint.PowerShell -Path $pathToModules
 
 #to import the module
-$versionUsed = "16.0.27011.12008"
+$versionUsed = "16.0.27313.12000"
 Import-Module "$pathToModules\Microsoft.Online.SharePoint.PowerShell\$versionUsed\Microsoft.Online.SharePoint.PowerShell.psd1" -UseWindowsPowerShell
 
 ## pnp powershell module
